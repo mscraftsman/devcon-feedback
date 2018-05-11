235250f25def2679b17d9cf360caf83fd3071812
Sandeep Ramgolam <sandeep@lsl.digital>
2018-05-11 16:31:52 +0400

    hide some links (look away!)

--- web/webapp-client/src/views/SessionSingle.vue
+++ web/webapp-client/src/views/SessionSingle.vue
@@ -1,379 +1,373 @@
 <template>
-    <div class="page page-session">
-        <div class="back-button-wrapper">
-            <router-link :to="{ name: 'sessions' }" class="back">
-                <img src="../assets/back.svg" alt="">
-            </router-link>
+  <div class="page page-session">
+    <div class="back-button-wrapper">
+      <router-link :to="{ name: 'sessions' }" class="back">
+        <img src="../assets/back.svg" alt="">
+      </router-link>
+    </div>
+    <div class="page-content" v-if="session">
+      <!-- <span>{{id}}</span> -->
+      <div class="session-title">{{session.title}}</div>
+
+      <div class="speakers-wrapper" v-if="session.speakers">
+        <div class="speaker-wrapper" v-for="speaker in session.speakers" :key="speaker.id">
+          <div class="avatar"><img :src="getSpeaker(speaker.id)" alt=""></div>
+          <p class="name">{{ speaker.name }}</p>
         </div>
-        <div class="page-content" v-if="session">
-            <!-- <span>{{id}}</span> -->
-            <div class="session-title">{{session.title}}</div>
+      </div>
 
-            <div class="speakers-wrapper" v-if="session.speakers">
-                <div class="speaker-wrapper" v-for="speaker in session.speakers" :key="speaker.id">
-                    <div class="avatar"><img :src="getSpeaker(speaker.id)" alt=""></div>
-                    <p class="name">{{ speaker.name }}</p>
-                </div>
-            </div>
-
-            <div class="descriptions-row">
-                <div class="des-wrap" v-if="session.format">
-                    <label>
-                        <img src="../assets/icons/language.svg" alt="">
-                    </label>
-                    <p>{{ session.format }}</p>
-                </div>
-
-                <div class="des-wrap" v-if="session.language">
-                    <label>
-                        <img src="../assets/icons/language.svg" alt="">
-                    </label>
-                    <p>{{ session.language }}</p>
-                </div>
-
-                <div class="des-wrap">
-                    <label>
-                        <img src="../assets/icons/location.svg" alt="">
-                    </label>
-                    <p>{{ session.room }}</p>
-                </div>
-
-                <div class="des-wrap">
-                    <label>
-                        <img src="../assets/icons/time.svg" alt="">
-                    </label>
-                    <p>{{ time(session.startsAt) }} - {{ time(session.endsAt) }}</p>
-                </div>
-
-                <div class="des-wrap" v-if="session.level">
-                    <label>
-                        <img src="../assets/icons/level.svg" alt="">
-                    </label>
-                    <p>{{ session.level }}</p>
-                </div>
-
-                <div class="des-wrap rate">
-                    <router-link :to="{ name: 'feedback',  params: { id: id }}" class="rate">
-                        Rate
-                    </router-link>
-                </div>
-            </div>
-
-            <div class="description-text">
-                <p>{{ session.description }}</p>
-            </div>
-
+      <div class="descriptions-row">
+        <div class="des-wrap" v-if="session.format">
+          <label>
+            <img src="../assets/icons/language.svg" alt="">
+          </label>
+          <p>{{ session.format }}</p>
         </div>
-        <div class="page-content" v-else>
-            calling Ish..
+
+        <div class="des-wrap" v-if="session.language">
+          <label>
+            <img src="../assets/icons/language.svg" alt="">
+          </label>
+          <p>{{ session.language }}</p>
         </div>
-        <div class="footer">
-            Developer Conference 2018
+
+        <div class="des-wrap">
+          <label>
+            <img src="../assets/icons/location.svg" alt="">
+          </label>
+          <p>{{ session.room }}</p>
         </div>
+
+        <div class="des-wrap">
+          <label>
+            <img src="../assets/icons/time.svg" alt="">
+          </label>
+          <p>{{ getDay(session.startsAt) }} {{ time(session.startsAt) }} - {{ time(session.endsAt) }}</p>
+        </div>
+
+        <div class="des-wrap" v-if="session.level">
+          <label>
+            <img src="../assets/icons/level.svg" alt="">
+          </label>
+          <p>{{ session.level }}</p>
+        </div>
+
+        <div class="des-wrap rate">
+          <!-- <router-link :to="{ name: 'feedback',  params: { id: id }}" class="rate">
+            Rate
+          </router-link> -->
+        </div>
+      </div>
+
+      <div class="description-text">
+        <p>{{ session.description }}</p>
+      </div>
 
     </div>
+    <div class="page-content" v-else>
+      calling Ish..
+    </div>
+    <div class="footer">
+      Developer Conference 2018
+    </div>
+
+  </div>
 </template>
 
 <script>
-    import { mapGetters, mapActions } from "vuex";
-    import moment from "moment";
+import { mapGetters, mapActions } from "vuex";
+import moment from "moment";
 
-    export default {
-        props: ["id"],
-        methods: {
-            ...mapActions(["fetchSessions", "fetchSpeakers"]),
-            getSpeaker: function(id) {
-                if (this.speakers.length === 0) {
-                    this.fetchSpeakers();
-                }
-                console.log(id);
-                console.log(this.speakers);
-                let theSpeaker = this.speakers.filter(speaker => speaker.id === id);
-                if (theSpeaker.length > 0) {
-                    return theSpeaker[0].profilePicture;
-                }
-            },
-            time: function(date) {
-                // console.log()
-                // return new Date(date).toDateString();
-                return moment(date).format("LT");
-            },
-            getDay: function(str) {
-                return str.split(",")[0];
-            }
-        },
-        computed: {
-            ...mapGetters({
-                sessions: "getSessions",
-                speakers: "getSpeakers"
-            }),
-            session: function() {
-                let sessions = this.sessions
-                    .map(groups => groups.sessions)
-                    .reduce(function(acc, curr) {
-                        return [...acc, ...curr];
-                    }, []);
-                let session = _.filter(sessions, { id: this.id })[0];
-                // console.log(session);
-                return session;
-            }
-        },
-        beforeMount() {
-            if (this.$store.state.sessions.length === 0) {
-                // console.error("no sessions found");
-                this.fetchSessions();
-            } else {
-                // console.info("sessions found !");
-            }
-        }
-    };
+export default {
+  props: ["id"],
+  methods: {
+    ...mapActions(["fetchSessions", "fetchSpeakers"]),
+    getSpeaker: function(id) {
+      if (this.speakers.length === 0) {
+        this.fetchSpeakers();
+      }
+      console.log(id);
+      console.log(this.speakers);
+      let theSpeaker = this.speakers.filter(speaker => speaker.id === id);
+      if (theSpeaker.length > 0) {
+        return theSpeaker[0].profilePicture;
+      }
+    },
+    time: function(date) {
+      // console.log()
+      // return new Date(date).toDateString();
+      return moment(date).format("LT");
+    },
+    getDay: function(str) {
+      return moment(str).format("ddd");
+    }
+  },
+  computed: {
+    ...mapGetters({
+      sessions: "getSessions",
+      speakers: "getSpeakers"
+    }),
+    session: function() {
+      let sessions = this.sessions
+        .map(groups => groups.sessions)
+        .reduce(function(acc, curr) {
+          return [...acc, ...curr];
+        }, []);
+      let session = _.filter(sessions, { id: this.id })[0];
+      // console.log(session);
+      return session;
+    }
+  },
+  beforeMount() {
+    if (this.$store.state.sessions.length === 0) {
+      // console.error("no sessions found");
+      this.fetchSessions();
+    } else {
+      // console.info("sessions found !");
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-    * {
-        box-sizing: border-box;
+* {
+  box-sizing: border-box;
+}
+
+.page-session {
+  display: grid;
+  grid-template-areas:
+    "back back"
+    "session session"
+    "footer footer";
+  grid-template-columns: 100px 1fr;
+  grid-template-rows: 50px auto;
+  grid-auto-rows: auto;
+  max-width: 900px;
+  margin: 0 auto;
+  width: 100%;
+  margin-top: 20px;
+}
+
+.back-button-wrapper {
+  grid-area: back;
+  text-align: left;
+
+  a {
+    display: flex;
+    align-items: center;
+    height: 40px;
+    width: 40px;
+    padding: 10px;
+    background: var(--color-blue);
+    border-radius: 40px;
+    padding-left: 13px;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.5);
+
+    img {
+      height: 100%;
+      margin-right: 15px;
+    }
+  }
+}
+
+.page-content {
+  grid-area: session;
+  background: white;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+}
+
+.session-title {
+  color: #333333;
+  text-transform: uppercase;
+  font-family: var(--font-glacial);
+  font-size: 40px;
+  font-weight: 700;
+  margin: 0 auto;
+  padding: 30px 10px;
+  text-align: center;
+}
+
+.speakers-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 20px;
+
+  .speaker-wrapper {
+    --width: 100px;
+    display: grid;
+    width: 200px;
+    grid-template-areas: "avatar name";
+    grid-template-columns: var(--width) 1fr;
+    align-items: center;
+    grid-gap: 10px;
+    margin-bottom: 10px;
+    margin-right: 20px;
+    font-family: var(--font-shentox);
+    font-weight: 500;
+    text-transform: uppercase;
+
+    &:last-child {
+      margin-right: 0;
     }
 
-    .page-session {
-        display: grid;
-        grid-template-areas:
-                "back back"
-                "session session"
-                "footer footer";
-        grid-template-columns: 100px 1fr;
-        grid-template-rows: 50px auto;
-        grid-auto-rows: auto;
-        max-width: 900px;
-        margin: 0 auto;
+    .avatar {
+      grid-area: avatar;
+      width: var(--width);
+      height: var(--width);
+      border-radius: var(--width);
+      box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+      overflow: hidden;
+
+      img {
+        width: var(--width);
+        height: var(--width);
+        border-radius: var(--width);
+        display: block;
+        object-position: 50% 50%;
+        object-fit: cover;
+      }
+    }
+
+    p {
+      grid-area: name;
+      text-align: left;
+      font-size: 15px;
+      color: var(--color-blue);
+      font-weight: 700;
+    }
+  }
+}
+
+.descriptions-row {
+  background: var(--color-blue);
+  width: 100%;
+  display: flex;
+  align-items: center;
+  font-size: 18px;
+
+  .des-wrap {
+    width: 33.3%;
+    margin-right: 10px;
+    color: white;
+    font-family: var(--font-glacial);
+    text-transform: uppercase;
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    height: 50px;
+
+    &:last-child {
+      margin-right: 0;
+    }
+
+    &.rate {
+      padding: 0;
+
+      a {
+        background: var(--color-green);
+        height: 50px;
         width: 100%;
-        margin-top: 20px;
-    }
-
-    .back-button-wrapper {
-        grid-area: back;
-        text-align: left;
-
-        a {
-            display: flex;
-            align-items: center;
-            height: 40px;
-            width: 40px;
-            padding: 10px;
-            background: var(--color-blue);
-            border-radius: 40px;
-            padding-left: 13px;
-            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.5);
-
-            img {
-                height: 100%;
-                margin-right: 15px;
-            }
-        }
-    }
-
-    .page-content {
-        grid-area: session;
-        background: white;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-    }
-
-    .session-title {
-        color: #333333;
-        text-transform: uppercase;
-        font-family: var(--font-glacial);
-        font-size: 40px;
-        font-weight: 700;
-        margin: 0 auto;
-        padding: 30px 10px;
-        text-align: center;
-    }
-
-    .speakers-wrapper {
         display: flex;
         align-items: center;
         justify-content: center;
-        margin-bottom: 20px;
-
-        .speaker-wrapper {
-            --width: 70px;
-            display: grid;
-            width: 200px;
-            grid-template-areas: "avatar name";
-            grid-template-columns: var(--width) 1fr;
-            align-items: center;
-            grid-gap: 10px;
-            margin-bottom: 10px;
-            margin-right: 20px;
-            font-family: var(--font-shentox);
-            font-weight: 500;
-            text-transform: uppercase;
-
-            &:last-child {
-                margin-right: 0;
-            }
-
-            .avatar {
-                grid-area: avatar;
-                width: var(--width);
-                height: var(--width);
-                border-radius: var(--width);
-                box-shadow: 0 0 20px rgba(0,0,0,.1);
-                overflow: hidden;
-
-                img {
-                    width: var(--width);
-                    height: var(--width);
-                    border-radius: var(--width);
-                    display: block;
-                    object-position: 50% 50%;
-                    object-fit: cover;
-                }
-            }
-
-            p {
-                grid-area: name;
-                text-align: left;
-                font-size: 15px;
-                color: var(--color-blue);
-                font-weight: 700;
-            }
-        }
-
+        text-decoration: none;
+        font-size: 20px;
+        color: var(--color-blue);
+      }
     }
 
-    .descriptions-row {
-        background: var(--color-blue);
-        width: 100%;
-        display: flex;
-        align-items: center;
-        font-size: 18px;
+    label {
+      margin-right: 10px;
 
-        .des-wrap {
-            width: 33.3%;
-            margin-right: 10px;
-            color: white;
-            font-family: var(--font-glacial);
-            text-transform: uppercase;
-            display: flex;
-            align-items: center;
-            padding: 10px;
-            height: 50px;
-
-            &:last-child {
-                margin-right: 0;
-            }
-
-            &.rate {
-                padding: 0;
-
-                a {
-                    background: var(--color-green);
-                    height: 50px;
-                    width: 100%;
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                    text-decoration: none;
-                    font-size: 20px;
-                    color: var(--color-blue)
-                }
-            }
-
-            label {
-                margin-right: 10px;
-
-                img {
-                    width: 20px;
-                }
-            }
-
-            p {
-                margin: 0;
-            }
-        }
-
+      img {
+        width: 20px;
+      }
     }
 
-    .description-text {
-        padding: 20px;
-
-        .back {
-
-        }
-
-        p {
-            font-family: var(--font-glacial);
-            font-size: 18px;
-            line-height: 25px;
-            font-weight: 300;
-        }
+    p {
+      margin: 0;
     }
+  }
+}
 
-    .button-wrappers {
-        display: grid;
-        grid-template-columns: 1fr;
-        grid-template-rows: 1fr;
+.description-text {
+  padding: 20px;
 
-        .back {
-            grid-column: 1 / 2;
-        }
+  .back {
+  }
 
+  p {
+    font-family: var(--font-glacial);
+    font-size: 18px;
+    line-height: 25px;
+    font-weight: 300;
+  }
+}
+
+.button-wrappers {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr;
+
+  .back {
+    grid-column: 1 / 2;
+  }
+}
+
+.footer {
+  grid-area: footer;
+  color: black;
+  text-transform: uppercase;
+  font-family: var(--font-shentox);
+  color: white;
+  text-align: center;
+  font-size: 15px;
+  align-self: center;
+  padding: var(--gutter) 0;
+}
+
+@media (max-width: 1000px) {
+  .back-button-wrapper {
+    padding: 0 10px;
+  }
+
+  .session-title {
+    font-size: 30px;
+  }
+}
+
+@media (max-width: 768px) {
+  .descriptions-row {
+    flex-wrap: wrap;
+
+    .des-wrap {
+      width: 100%;
     }
+  }
 
-    .footer {
-        grid-area: footer;
-        color: black;
-        text-transform: uppercase;
-        font-family: var(--font-shentox);
-        color: white;
-        text-align: center;
-        font-size: 15px;
-        align-self: center;
-        padding: var(--gutter) 0;
+  .speakers-wrapper {
+    flex-wrap: wrap;
+
+    .speaker-wrapper {
+      --width: 40px;
+      width: 100%;
+
+      margin-right: 0;
+      padding: 0 10px;
+
+      .name {
+        font-size: 13px;
+      }
     }
+  }
+}
 
-    @media (max-width: 1000px) {
-        .back-button-wrapper {
-            padding: 0 10px;
-        }
-
-        .session-title {
-            font-size: 30px;
-        }
-    }
-
-    @media (max-width: 768px) {
-        .descriptions-row {
-            flex-wrap: wrap;
-
-
-            .des-wrap {
-                width: 100%;
-            }
-        }
-
-        .speakers-wrapper {
-            flex-wrap: wrap;
-
-            .speaker-wrapper {
-                --width: 40px;
-                width: 100%;
-
-                margin-right: 0;
-                padding: 0 10px;
-
-                .name {
-                    font-size: 13px;
-                }
-            }
-        }
-    }
-
-    @media (max-width: 480px) {
-        .session-title {
-            font-size: 25px;
-        }
-    }
-
+@media (max-width: 480px) {
+  .session-title {
+    font-size: 25px;
+  }
+}
 </style>
 
