14b6fd3be0d85e953de249f98ea810d0c775980a
Yusuf Satar <yusuf@lsl.digital>
2018-05-16 21:51:30 +0400

    feedback validation + schema update

--- models/feedback/feedback_rest_hooks.gocipe.go
+++ models/feedback/feedback_rest_hooks.gocipe.go
@@ -9,6 +9,32 @@ import (
 	"github.com/mscraftsman/devcon-feedback/util"
 )
 
+func areResponsesInvalid(entity *Feedback) bool {
+	if entity.Reaction1 == nil || entity.Reaction2 == nil || entity.Reaction3 == nil || entity.Reaction4 == nil {
+		return true
+	}
+
+	switch *entity.Reaction1 {
+	case "-2", "-1", "1", "2", "3":
+	default:
+		return true
+	}
+
+	switch *entity.Reaction2 {
+	case "-2", "-1", "1", "2", "3":
+	default:
+		return true
+	}
+
+	switch *entity.Reaction2 {
+	case "no", "yes":
+	default:
+		return true
+	}
+
+	return false
+}
+
 func restPreCreate(w http.ResponseWriter, r *http.Request, entity *Feedback, tx *sql.Tx) (bool, error) {
 	visitor, err := meetup.DecodeToken(r)
 
@@ -17,6 +43,9 @@ func restPreCreate(w http.ResponseWriter, r *http.Request, entity *Feedback, tx 
 		return true, nil
 	} else if entity.SessionID == nil || !sessionize.IsValidSession(*entity.SessionID) {
 		util.JSONOutputError(w, http.StatusForbidden, "invalid session")
+		return true, nil
+	} else if areResponsesInvalid(entity) {
+		util.JSONOutputError(w, http.StatusBadRequest, "incomplete feedback")
 		return true, nil
 	}
 
